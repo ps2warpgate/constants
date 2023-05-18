b6b017dab7d85b81871670d580e0c7786b983892
wupasscat <wupasscat10@gmail.com>
2023-05-18 18:04:02 -0400

    use model for alert

---
+++ typings/FieldTypes.py
@@ -0,0 +1,7 @@
+"""Common Event attribute types"""
+
+from typing import Literal
+
+
+World = Literal[1, 10, 13, 17, 19, 40]
+Zone = Literal[2, 4, 6, 8, 344]
--- typings/__init__.py
+++ typings/__init__.py
@@ -1,9 +1,10 @@
 """Custom types"""
 
-
+from .FieldTypes import World, Zone
 from .UniqueEventId import UniqueEventId
-
 
 __all__ = [
     'UniqueEventId',
+    'World',
+    'Zone',
 ]
