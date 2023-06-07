a012d188f788e7511bed65f59db2240de42da1e9
wupasscat <wupasscat10@gmail.com>
2023-06-07 13:13:59 -0400

    classes are cool

--- typings/UniqueEventId.py
+++ typings/UniqueEventId.py
@@ -1,16 +1,4 @@
-class UniqueEventId:
-    def __init__(self, world_id: int, instance_id: int) -> None:
-        """Combines world_id and instance_id to create a unique event id
-
-        Args:
-            - world_id: `int` Event world_id
-            - instance_id: `int` Event instance_id
-        """
-        self.world_id = world_id
-        self.instance_id = instance_id
-
-    def __str__(self) -> str:
-        return f'{self.world_id}-{self.instance_id}'
-    
-    def __repr__(self) -> str:
-        return f'UniqueEventId(world_id={self.world_id}, instance_id={self.instance_id})'
+class UniqueEventId(str):
+    def __new__(cls, world_id: int, instance_id: int):
+        new_event_id = f'{world_id}-{instance_id}'
+        return super().__new__(cls, new_event_id)
